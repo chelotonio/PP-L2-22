7edbdebdc3ddae6996c5e09b4b6d9cab2b256917
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-04-27 23:09:41 -0400

    Iniciación de TDA cardsSet

--- TDA_cardsSet.pl
+++ TDA_cardsSet.pl
@@ -1,7 +1,14 @@
 % TDA cardsSet
+% TDA que representa la baraja de cartas que se utiliza en el juego Dobble.
+% Representación: Listas.
 
-% cardsSet().
+% Hechos.
 
-% Hechos y reglas.
+% Reglas.
+
+cardsSet(Elements, numE, maxC, seed, CS).
+% Ejemplo de uso:
+% cardsSet([1, 2, 3, 4, 5, 6, 7, … ], 3, 5, 92175, CS).
+% CS = [[1, 2, 3], [1, 4, 5], [1, 7, 8], ...]
 
 % Consultas.
