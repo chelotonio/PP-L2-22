7a14819fa1c7d80d9815168aa3b8bda5e39f139b
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-05-09 06:25:58 -0400

    Creación de primeras n cartas

--- TDA_cardsSet.pl
+++ TDA_cardsSet.pl
@@ -19,9 +19,28 @@ cardsSet(Elements, NumE, MaxC, Seed, CS).
 % lista vacía.
 % [] -> [[1, 2, 3]]
 
+% firstCard es una lista con la primera carta
+% primeraCarta(5, [], A).
 primeraCarta(0,	FirstCard, FirstCard).
 primeraCarta(NumE, Resto, FirstCard) :-
     NumE_i is NumE - 1,
     primeraCarta(NumE_i, [NumE|Resto], FirstCard), !.
+
+% Genera una de las n primeras cartas.
+% n_cartas(1, 1, 3, [1], A).
+n_cartas(_, NumE, NumE, CartaN, CartaN).
+n_cartas(_j, _k, NumE, Resto, CartaN):-
+    N is NumE - 1,
+    Num is N * _j + (_k + 1),
+    New_k is _k + 1,
+    n_cartas(_j, New_k, NumE, [Num|Resto], CartaN), !.
+
+% Almacena las primeras n cartas en baraja.
+n_cartas_in(NumE, NumE, Baraja, Baraja).
+n_cartas_in(_j, NumE, BarajaAct, B):-
+    n_cartas(_j, 1, NumE, [1], CartaN),
+    insertar_en_baraja(CartaN, BarajaAct, BarajaPst),
+    New_j is _j + 1,
+    n_cartas_in(New_j, NumE, BarajaPst, B), !.
 
 % Consultas.
