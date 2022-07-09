48d7fa17955d8b27dbc9dd759f560cd476b9b712
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-07-09 12:37:51 -0400

    Ordenamiento de código

--- TDA cardsSet.pl
+++ TDA cardsSet.pl
@@ -1,53 +1,33 @@
-%cardsSet(Elements, NumE, MaxC, Seed, CS).
-% Ejemplo de uso:
-% cardsSet([1, 2, 3, 4, 5, 6, 7, … ], 3, 5, 92175, CS).
-% CS = [[1, 2, 3], [1, 4, 5], [1, 7, 8], ...]
+% TDA CardsSet
+% Representación computacional de la generación de cartas
+% con sus respectivos símbolos, respetando las restricciones
+% correspondientes del juego Dobble.
 
-% Aplicar recursión para generar primera carta.
-% Tengo una lista que representa el mazo.
-% Se debe crear recursión que genere primera carta y la almacene en la
-% lista vacía.
-% [] -> [[1, 2, 3]]
+% Representación: Listas
 
+% Posibles Elements:
+% [1, 2, 3, 4, 5, 6, 7]
+% ["A", "B", "C", "D","E", "F", "G"]
+% ["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"]
+
+% _____________ TDA CardsSet - Constructor _____________
+
+% Regla que inserta un elemento dado a una carta.
 insertar_en_baraja(Carta, [], [Carta]).
 insertar_en_baraja(Carta, Baraja, [Carta|Baraja]).
 
+% Regla que genera una lista a partir de la unión de otras dos.
 join( [], Lista, Lista ).
 join( [CabezaL1|RestoL1], Lista2, [CabezaL1|ListaResultado] ) :-
 	join( RestoL1, Lista2, ListaResultado ).
 
-% firstCard es una lista con la primera carta
-% primeraCarta(5, [], A).
+% Regla que genera la primera carta.
 primeraCarta(0,	FirstCard, FirstCard).
 primeraCarta(NumE, Resto, FirstCard) :-
     NumE_i is NumE - 1,
     primeraCarta(NumE_i, [NumE|Resto], FirstCard), !.
     
-% Genera una de las n primeras cartas.
-% n_cartas(1, 1, 3, [1], A).
-n_cartas(_, NumE, NumE, CartaN, CartaN).
-n_cartas(_j, _k, NumE, Resto, CartaN):-
-    N is NumE - 1,
-    Num is N * _j + (_k + 1),
-    New_k is _k + 1,
-    n_cartas(_j, New_k, NumE, [Num|Resto], CartaN), !.
-
-% Almacena las primeras n cartas en baraja.
-% n_cartas_in(1, 4, [], B).
-n_cartas_in(NumE, NumE, Baraja, Baraja).
-n_cartas_in(_j, NumE, BarajaAct, B):-
-    n_cartas(_j, 1, NumE, [1], CartaN),
-    insertar_en_baraja(CartaN, BarajaAct, BarajaPst),
-    New_j is _j + 1,
-    n_cartas_in(New_j, NumE, BarajaPst, B), !.
-    
-
-% Genera una de las n cuadrado primeras cartas.
-
-% __________1ras n cartas______________
-
-% generar_n_cartas(1, 3, [], A)
-% for(1, 4, [], A).
+% Regla que genera una lista con las primeras n cartas de cardsSet.
 generar_n_cartas(_n, _n, Lista, Lista).
 generar_n_cartas(_i, _n, Lista, Salida):-
     n_carta(_i, 1, _n, [1], Carta),
@@ -55,8 +35,7 @@ generar_n_cartas(_i, _n, Lista, Salida):-
     New_i is _i + 1, 
     generar_n_cartas(New_i, _n, Lista2, Salida), !.
 
-% generar_n_cartas(1, 3, [], A).
-% for2(1, 1, 3, [1], A).
+% Regla que construye cada carta con la estructura de las primeras n cartas de cardsSet.
 n_carta(_, _n, _n, Lista, Lista).
 n_carta(_i, _j, _n, Lista, Salida):-
     New_n is _n - 1,
@@ -65,15 +44,14 @@ n_carta(_i, _j, _n, Lista, Salida):-
     New_j is _j + 1, 
     n_carta(_i, New_j, _n, Lista2, Salida), !.
 
-% ____________1ras n2 cartas____________
-
+% Regla que genera una lista con las últimas n2 cartas de cardsSet.
 generar_n2_cartas(_n, _n, Lista, Lista).
 generar_n2_cartas(_i, _n, Lista, Salida):-
     n2_carta(_i, 1, _n, Lista, Carta),
     New_i is _i + 1, 
     generar_n2_cartas(New_i, _n, Carta, Salida), !.
 
-% for2(1, 1, 3, [1], A).
+% Regla que genera una lista con parte de las últimas n2 cartas de cardsSet.
 n2_carta(_, _n, _n, Lista, Lista).
 n2_carta(_i, _j, _n, Lista, Salida):-
     First_i is _i + 1,
@@ -82,6 +60,7 @@ n2_carta(_i, _j, _n, Lista, Salida):-
     New_j is _j + 1, 
     n2_carta(_i, New_j, _n, Lista2, Salida), !.
 
+% % Regla que construye cada carta con la estructura de las últimas n2 cartas de cardsSet.
 n2_cartas(_, _, _n, _n, Lista, Lista).
 n2_cartas(_i, _j, _k, _n, Lista, Salida):-
     New_n is _n - 1,
@@ -90,18 +69,25 @@ n2_cartas(_i, _j, _k, _n, Lista, Salida):-
     New_k is _k + 1, 
     n2_cartas(_i, _j, New_k, _n, Lista2, Salida), !.
 
-% ____________cardsSet_____________
-
 % cardsSet(3, NumE, MaxC, Seed, CS).
 cardsSet(Elements, NumE, MaxC, Seed, CS):-
-primeraCarta(Elements, [], A),
-insertar_en_baraja(A, [], B),
-generar_n_cartas(1, Elements, [], C),
-generar_n2_cartas(1, Elements, [], D),
-join(B, C, E),
-join(E, D, CS), !.
+  primeraCarta(Elements, [], A),
+  insertar_en_baraja(A, [], B),
+  generar_n_cartas(1, Elements, [], C),
+  generar_n2_cartas(1, Elements, [], D),
+  join(B, C, E),
+  join(E, D, CS), !.
+
+_____________ TDA CardsSet - dobble _____________
 
 
 
 
-% Consultas.
+% Consultas de ejemplo para cardsSet:
+%
+%
+%
+
+
+
+
