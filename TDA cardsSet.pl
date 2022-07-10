763670c64c264f44299c6f48e61486da50f9c25e
Marcelo Vásquez <marcelo.vasquez.s@usach.cl>
2022-07-10 17:52:05 -0400

    Creación de cardsSetIsDobble

--- TDA cardsSet.pl
+++ TDA cardsSet.pl
@@ -10,7 +10,17 @@
 % ["A", "B", "C", "D","E", "F", "G"]
 % ["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"]
 
+myRandom(Xn, Xn1):-
+AX is 1103515245 * Xn,
+AXC is AX + 12345,
+Xn1 is (AXC mod 2147483647).
+
 % _____________ TDA CardsSet - Constructor _____________
+
+contarLista([], _i, _i).
+contarLista([_|Resto], _i, Salida):-
+    New_i is _i + 1,
+    contarLista(Resto, New_i, Salida).
 
 % Regla que inserta un elemento dado a una carta.
 insertar_en_baraja(Carta, [], [Carta]).
@@ -105,8 +115,54 @@ cardsSet(Elements, NumE, MaxC, Seed, CS):-
 
 % _____________ TDA CardsSet - dobble _____________
 
+% Regla que cuenta la cantidad de veces que se repite un elemento en una lista.
+contarRepetidosAux(_, [], Repetidos, Repetidos).
+contarRepetidosAux(E, [E|Resto], Cuenta, Salida):-
+	N_cuenta is Cuenta + 1,
+    contarRepetidosAux(E, Resto, N_cuenta, Salida), !.
+contarRepetidosAux(E, [_|Resto], Cuenta, Salida):-
+    contarRepetidosAux(E, Resto, Cuenta, Salida), !.
+
+% Regla que cuenta la cantidad de elementos de una carta que se repiten en otra carta.
+contarRepetidos([], _, SumaCuentas, SumaCuentas).
+contarRepetidos([Elemento|Resto], Carta2, SumaCuentas, Salida):-
+    contarRepetidosAux(Elemento, Carta2, 0, Cuenta),
+    Ncuenta is SumaCuentas + Cuenta,
+    contarRepetidos(Resto, Carta2, Ncuenta, Salida), !.
+contarRepetidos([_|[]], _, SumaCuentas, Salida):-
+    contarRepetidos([], _, SumaCuentas, Salida), !.
+
+% Regla que permite verificar si el conjunto de cartas en el TDA corresponden a un conjunto válido.
+% Dominio: CardsSet (Lista)
+% Salida: Boolean
+cardsSetIsDobble([]).
+cardsSetIsDobble([Carta1|Resto]):-
+    buscarElemento(Resto, 0, Carta2),
+    contarRepetidos(Carta1, Carta2, 0, Cuenta),
+    Cuenta = 1,
+    cardsSetIsDobble(Resto), !.
+cardsSetIsDobble([_|[]]):-
+    cardsSetIsDobble([]), !.
 
 % _____________ TDA CardsSet - nthCard _____________
+
+% Regla que obtiene la n-ésima (nth) carta desde el conjunto de cartas partiendo desde 0 hasta (totalCartas-1).
+% Dominio: CardsSet (lista) X n (int) X Card (lista)
+% Recorrido: Card (lista)
+cardsSetNthCard(CardsSet, N, Card):-
+    NewN is N - 1,
+    buscarElemento(CardsSet, NewN, Card), !.
+
+% _____________ TDA CardsSet - nthCard _____________
+
+% Regla que a partir de una carta de muestra, determina la cantidad total de cartas que se deben producir para construir un conjunto válido.
+% Dominio:
+% Recorrido: 
+cardsSetFindTotalCards(Card, Total):-
+    contarLista(Card, 0, A), 
+    Total is ((A - 1) * (A - 1)) + (A - 1) + 1.
+
+% _____________ TDA CardsSet - missingCards _____________
 
 
 
@@ -116,3 +172,31 @@ cardsSet(Elements, NumE, MaxC, Seed, CS):-
 % cardsSet(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], 4, 10, 912321, CS).
 % cardsSet(["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"], 3, -1, 84392, CS).
 % cardsSet([1, 2, 3, 4, 5, 6, 7], 2, 3, 912321, CS).
+
+
+% Consultas de ejemplo para dobble?:
+% CS = [[1, 2, 3], [1, 4, 5], [1, 6, 7], [1, 7, 8]], cardsSetIsDobble(CS). % CardsSet inválido
+
+% Para un caso verdadero, prolog retorna por defecto el propio cardsSet que se genera: 
+% cardsSet(["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"], 3, 3,84392, CS), cardsSetIsDobble(CS).
+% Para recibir True se ingresa el maso creado por cardsSet directamente: 
+% cardsSetIsDobble([["Lana", "Zorro", "Arból"], ["Silla", "Cama", "Arból"], ["Arból", "Manzana", "Plátano"]]).
+
+% Para un caso verdadero, prolog retorna por defecto el propio cardsSet que se genera: 
+% cardsSet(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], 4, 4, 912321, CS), cardsSetIsDobble(CS).
+% Para recibir True se ingresa el maso creado por cardsSet directamente: 
+% cardsSetIsDobble([["G", "F", "E", "A"], ["J", "I", "H", "A"], ["M", "L", "K", "A"], ["A", "B", "C", "D"]]). 
+
+% Consultas de ejemplo para nthCard:
+% cardsSet([1, 2, 3, 4, 5, 6, 7], 2, 3, 912321, CS), cardsSetNthCard(CS, 3, Card3).
+% cardsSet(["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"], 3, -1, 84392, CS), cardsSetNthCard(CS, 6, Card6).
+% cardsSet(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], 4, 10, 912321, CS), cardsSetNthCard(CS, 9, Card9).
+
+% Consultas de ejemplo para findTotalCards:
+% cardsSet(["Arból", "Manzana", "Plátano", "Zorro", "Lana", "Cama", "Silla"], 3, -1, 84392, CS), cardsSetNthCard(CS, 6, Card6), cardsSetFindTotalCards(Card6, Total).
+% cardsSet(["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"], 4, 10, 912321, CS), cardsSetNthCard(CS, 9, Card9), cardsSetFindTotalCards(Card9, Total).
+% cardsSet([1, 2, 3, 4, 5, 6, 7], 2, 2, 912321, CS), cardsSetNthCard(CS, 2, Card2), cardsSetFindTotalCards(Card3, Total).
+
+% Consultas de ejemplo para missingCards:
+
+
